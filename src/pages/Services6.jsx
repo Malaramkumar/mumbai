52591a1826cb9f08f50a62eb08408672b8028d66
Malaramkumar <kumar.narpavitech@gmail.com>
2025-11-01 11:24:17 +0530

    s6

--- src/pages/Services6.jsx
+++ src/pages/Services6.jsx
@@ -1,43 +1,31 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  BookOpenCheck,
-  FileText,
-  FilePenLine,
-  Presentation,
-} from "lucide-react";
-
+import { BookOpenCheck, FileText, FilePenLine, Presentation } from "lucide-react";
 import "./Services6.scss";
-//import bgImage from "../images/bgmain3.png";
-
-import bgImage from "../images/PublicationSupport.png"
+import bgImage from "../images/PublicationSupport.png";
 
 const services = [
   {
     title: "Journal Publication Services",
     desc: "Get end-to-end assistance with journal selection, formatting, and submission.",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, earum.",
     link: "/career-opportunities-after-phd",
     icon: <BookOpenCheck size={24} />,
   },
   {
     title: "Research Publication Services",
     desc: "Professional guidance for publishing your original research with precision.",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, earum.",
     link: "/top-mnc-hiring",
     icon: <FileText size={24} />,
   },
   {
     title: "Review Paper Publication Support",
     desc: "Expert assistance in writing and structuring your review papers.",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, earum.",
     link: "/top-phd-colleges-mumbai",
     icon: <FilePenLine size={24} />,
   },
   {
     title: "Conference Paper Publication Services",
     desc: "Comprehensive support for conference paper writing and presentations.",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, earum.",
     link: "/top-for-phd-colleges-in-mumbai",
     icon: <Presentation size={24} />,
   },
@@ -79,6 +67,7 @@ const Services6 = () => {
         className="services6-dark-section"
         style={{ backgroundImage: `url(${bgImage})` }}
       >
+        {/* ===== Center Content ===== */}
         <div className="services6-center-content">
           <h1>Publication Support</h1>
           <p>
@@ -89,13 +78,24 @@ const Services6 = () => {
             implementation, and thesis development, we ensure academic
             excellence with precision and professionalism.
           </p>
-           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa rem animi velit tenetur eaque, deleniti culpa nam, vel architecto incidunt cum nesciunt sint, ea aperiam illo id vero odit iure dicta adipisci! Voluptatibus mollitia sunt obcaecati possimus quas, inventore, cupiditate laboriosam veniam maxime sit cumque, quaerat animi. Minus voluptates tenetur mollitia, numquam quam eligendi perferendis nihil eius quos magni, sed facilis! Debitis, nam in sunt dolorum, eum illum consequuntur vel vitae aspernatur suscipit velit ducimus recusandae? Facere consequatur nihil, quos omnis culpa expedita eveniet distinctio labore iste quod. Quis esse expedita tenetur laborum deleniti aliquam commodi delectus! Blanditiis, ipsum ullam.
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa rem animi velit
+            tenetur eaque, deleniti culpa nam, vel architecto incidunt cum nesciunt sint,
+            ea aperiam illo id vero odit iure dicta adipisci! Voluptatibus mollitia sunt
+            obcaecati possimus quas, inventore, cupiditate laboriosam veniam maxime sit
+            cumque, quaerat animi. Minus voluptates tenetur mollitia, numquam quam
+            eligendi perferendis nihil eius quos magni, sed facilis! Debitis, nam in sunt
+            dolorum, eum illum consequuntur vel vitae aspernatur suscipit velit ducimus
+            recusandae? Facere consequatur nihil, quos omnis culpa expedita eveniet
+            distinctio labore iste quod. Quis esse expedita tenetur laborum deleniti
+            aliquam commodi delectus! Blanditiis, ipsum ullam.
           </p>
           <button onClick={() => navigate("/phd-consulting")}>
             Publication Support
           </button>
         </div>
 
+        {/* ===== Service Titles (Tabs) ===== */}
         <div className="services6-title-row">
           {services.map((service, index) => (
             <div
@@ -112,12 +112,14 @@ const Services6 = () => {
           ))}
         </div>
 
+        {/* ===== Service Cards ===== */}
         <div className="services6-card-row">
           {services.map((service, index) => (
             <div
               key={index}
               className={`services6-service-card ${
-                index === currentIndex ? "active" : "" }`}
+                index === currentIndex ? "active" : ""
+              }`}
               onClick={() => navigate(service.link)}
             >
               <div className="services6-icon-circle">{service.icon}</div>
@@ -134,4 +136,3 @@ const Services6 = () => {
 };
 
 export default Services6;
-
